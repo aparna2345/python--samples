d28d448a75615e6a1a92f040266b7832df3eff2f
Aparna S <aparnaappuabu@gmail.com>
2017-05-09 16:26:09 +0530

    even numbers

---
+++ sample14.py
@@ -0,0 +1,12 @@
+i= 0
+a = input("enter the number:")
+numbers = []
+while i < int(a):
+    print(f"enter number {i}:",end =' ')
+    i=i+1
+    b=input()
+    b= int(b)
+    numbers.append(b)
+for number in numbers:
+    if number % 2 == 0:
+        print(f"the even number {number}")
